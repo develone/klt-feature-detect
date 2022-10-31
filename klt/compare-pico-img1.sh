50101a0a572b4a7735006cf77343ae8279181bd9
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-31 06:00:24 -0600

    used to compare RPi with pico

---
+++ klt/compare-pico-img1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ~/rp2040-freertos-project/test-images/img1.pgm img0.pgm
+cp ~/rp2040-freertos-project/test-images/img1.pgm img1.pgm
+./example2
+less feat1.txt 
+ 
+
---
+++ klt/compare-pico-lena.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ~/rp2040-freertos-project/test-images/l64.pgm img0.pgm
+cp ~/rp2040-freertos-project/test-images/l64.pgm img1.pgm
+./example2
+less feat1.txt 
+ 
+
